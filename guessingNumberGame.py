bc39edd8f32700067a759e14f869ccdca1ca9b20
Kirill Kirillov <kki43@uclive.ac.nz>
2017-07-15 13:31:22 +1200

    Close to finishing

--- guessingNumberGame.py
+++ guessingNumberGame.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
 from random import *
-from sys import *
+import sys
+
 
 def print_instructions():
     """Void; Prints out the instructions when the program starts to run"""
     #
-    #TODO: Add instructions to be printed
+    # TODO: Add instructions to be printed
 
 
 def generate_random_num(num1, num2):
     """Int; generates a random number in a specified range, default values are 
     between 0 and 1000"""
-    return randint(num1,num2)
+    return randint(num1, num2)
 
 
 def check_guess(guess):
     """Boolean; Checks the guess to match the number that needs to be guessed. 
-    Returns true is the nubmer is correct"""
+    Returns true is the nunber is correct"""
     #
-    #TODO: Add method to check if the number is guessed
+    # TODO: Add method to check if the number is guessed
     
     
 def check_is_num(user_input):
@@ -27,60 +28,107 @@ def check_is_num(user_input):
         return True
     else:
         return False
-    
-    
+
+
+def get_num_guesses():
+    """Integer; gets the number of guesses that the user will have for the game"""
+    num_guesses = input("How many guesses would you like? You can choose between 1 and 10")
+    check_terminate(num_guesses)
+
+    is_valid = check_is_valid_num(num_guesses)
+    while not is_valid:
+        print("Looks like that number is not valid")
+
+
 def check_terminate(user_input):
     """Boolean; Checks whether the input is -1 and if it is, the 
     program terminates"""
-    if user_input == int(-1):
-        return True
+    if user_input == "-1":
+        print("Goodbye friend and good luck!")
+        sys.exit()
     else:
-        return False
+        pass
     
     
 def check_is_valid_num(user_input):
     """Boolean; Checks whether the input is non-negative"""
-    if int(user_input) >= 0:
+    is_digit = check_is_num(user_input)
+    if int(user_input) >= 0 and is_digit:
         return True
     else:
         return False
 
 
+def get_number(to_find):
+    """Integer; Gets the user to input a number"""
+    if to_find == "low":
+        to_print0 = "Please enter a number for the lowest value to be: "
+        to_print1 = "Please enter a number for the lowest value to be. Enter -1 to terminate: "
+    elif to_find == "high":
+        to_print0 = "Please enter a number for the highest value to be: "
+        to_print1 = "Please enter a number for the highest value to be. Enter -1 to terminate: "
+
+    num = input(to_print1).strip()
+
+    # Check to terminate program
+    check_terminate(num)
+
+    # Check the input is actually a number
+    is_num = check_is_num(num)
+    while not is_num:
+        print("You did not enter a correct number. Please try again!")
+        num = input(to_print0)
+        is_num = check_is_num(num)
+
+    # Checks that the number is valid. Needs to be a natural number where 0 is included
+    is_valid = check_is_valid_num(num)
+
+    while not is_valid:
+        print("You did not enter a valid number. It needs to be a natural number.")
+        num = input(to_print0)
+        is_valid = check_is_valid_num(num)
+
+    return num
+
+
 def main():
     """Starts to run the program"""
-    #Initialise variables
-    num1 = 0
-    num2 = 1000
-    num1 = input("Please enter a number for the lowest value to be. Enter -1 to terminate: ")
-    
-    #Check the input is actually a number
-    is_num = check_is_num(num1)
-    
-    #Check to terminate program
-    if check_terminate(num1):
-        print("Goodbye friend and goodluck!")
-        system.exit()
-    
-    #Checks that the number is valid. Needs to be a natural number where 0 is included    
-    is_valid = check_is_valid_num(num1)
-    
-    #While an incorrect input is given, keep asking until a valid one is given
-    # or until the program is told to terminate
-    ##TODO: Refactor this to have it also check the second number
-    while is_valid == False:
-        num1 = input("Please enter a number for the lowest value to be: ")
-        is_num = check(num1)
-        if check_terminate(num1):
-            print("Goodbye friend and goodluck!")
-            system.exit()
-        is_valid = check_is_valid_num(num1)
-        
-        
-    
-        
+    # Initialise variables
+    num1 = get_number("low")
+    num2 = get_number("high")
+    LINE = "-"*30
+    guess_num = 1
+
     number_to_guess = generate_random_num(int(num1), int(num2))
-        
+    print("Generating a random number from the range {} - {}....".format(num1, num2))
     print(number_to_guess)
-        
+
+    while guess_num:
+        print(LINE)
+        print("Guess number: {} ".format(guess_num))
+        guess = input("Please enter your guess: ")
+
+        is_valid = check_is_valid_num(guess)
+        while not is_valid:
+            print("You did not enter a valid guess. Please try again!")
+            guess = input("Please enter your guess: ")
+            is_valid = check_is_valid_num(guess)
+
+        guess = int(guess)
+        if check_guess(guess):
+            print("Wow! You guessed this correctly. The number was {}.".format(number_to_guess))
+        elif guess_num :
+            if guess > number_to_guess:
+                guess_direction = "LOWER"
+            else:
+                guess_direction = "HIGHER"
+            print("Nope! That was not it. Try guessing {} this time.".format(guess_direction))
+            # guess_num += 1
+
+    print(LINE)
+    print("Hah! Looks like you're out of guesses....")
+    print("I win xD")
+    print("This is a true showing of why humans are inferior to the computer overlords!")
+
 if __name__ == "__main__":    
     main()
